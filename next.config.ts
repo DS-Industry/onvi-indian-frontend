b1a14f4e47acaaa8487e3c5ea14c73a8a83753f5
devang-garg <devang@moy-ka.com>
2025-05-28 11:12:25 +0530

    Prepare for gh-pages deployment.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // important for GH Pages
+  },
+  basePath: '/onvi-indian-frontend',
+  assetPrefix: '/onvi-indian-frontend/',
 };
 
 export default nextConfig;
